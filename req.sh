b12fd2f3939ebe366a0b1456b08b4ae1b9754626
Vitor <115955738+vitormag-gh@users.noreply.github.com>
2023-05-09 23:27:19 -0400

    Update req.sh

--- req.sh
+++ req.sh
@@ -1,2 +1,9 @@
 pip install --upgrade pip
-pip install requirements.txt
+pip install pandas==0.23.1
+pip install matplotlib
+pip install seaborn
+pip install numpy
+pip install scipy==1.0.0
+pip install scikit-learn==0.19.1
+pip install numpy==1.14.5
+
